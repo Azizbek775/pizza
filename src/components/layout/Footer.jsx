e805e020ad710a7e981fd4f37cd738baa6fabf56
Azizbek775 <azizbekadhamlonov775>
2025-11-19 16:45:11 +0500

    finished

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -45,6 +45,8 @@ const Footer = () => {
           <p>VK</p>
         </div>
 
+        
+
         <div className='mt-6 flex flex-col gap-3 md:flex-row md:items-center md:justify-between'>
           <p className='text-center text-gray-600 md:text-left'>{t('footer.address')}</p>
           <button className='w-full rounded-3xl bg-gray-200 py-3 px-6 font-semibold md:w-auto'>{t('footer.callRequest')}</button>
